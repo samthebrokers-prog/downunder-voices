edd9abb9a5ceb28342a6f85a65d4d9784f999cac
samthebrokers-prog <samthebrokers@gmail.com>
2026-09-01 01:26:09 +1200

    Remove stale Secret Harbour live banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata, Viewport } from 'next'
 import { Analytics } from '@vercel/analytics/next'
 
-import { LiveElectionBanner } from '@/components/live-election-banner'
 import { MainHeader } from '@/components/main-header'
 import { SiteFooter } from '@/components/site-footer'
 import './globals.css'
@@ -87,7 +86,6 @@ export default function RootLayout({
 
       <body>
         <MainHeader />
-        <LiveElectionBanner />
 
         <main>{children}</main>
 
